0c19d4aa66fa7620f144c334ac2cd08b1ab1a7f2
Mohamed Saad Ibn Seddik <ms.ibnseddik@gmail.com>
2026-02-16 00:59:56 -0500

    Remove unused script tag from BookmarkEmpty component

--- src/lib/components/BookmarkEmpty.svelte
+++ src/lib/components/BookmarkEmpty.svelte
@@ -1,6 +1,3 @@
-<script lang="ts">
-</script>
-
 <div class="text-center text-gray-400 py-8">
 	<svg class="w-12 h-12 mx-auto mb-3 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24">
 		<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
